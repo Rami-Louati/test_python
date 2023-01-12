88490002aca14db890e40e05d787409d19f3f5b3
Rami-Louati <122232791+Rami-Louati@users.noreply.github.com>
2023-01-12 15:54:40 +0100

    Update user_functions.py

--- user_functions.py
+++ user_functions.py
@@ -4,11 +4,11 @@ def get_email_from_input():
     return input("Write down your email: ")
 
 # More elaborated version (copy to src/user_functions.py)
-#def get_email_from_input():
- #   """ Contains '@' and '.' """
-  #  email = input("Write down your email: ")
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Write down your email: ")
 
-   # if ("@" not in email or "." not in email):
-    #    print('Email is not valid.')
-    #else:
-     #   return email
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.')
+    else:
+        return email
